7e004b10e5e1b462d343cd0418283652d3bdac71
Tushar Samaniya <tusharsamaniya4956@gmail.com>
2025-09-27 02:23:41 +0530

    this is cunstomers class under customer package

---
+++ customer/Customers.java
@@ -0,0 +1,23 @@
+package com.customer;
+
+public class Customers {
+	
+	String name;
+	String Street;
+	String area;
+	String pincode;
+	String mobileNumber;
+	
+
+	public Customers(String name, String street, String area, String pincode, String mobileNumber) {
+		super();
+		this.name = name;
+		Street = street;
+		this.area = area;
+		this.pincode = pincode;
+		this.mobileNumber = mobileNumber;
+	}
+	
+	
+
+}
